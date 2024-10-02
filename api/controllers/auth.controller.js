e2ff3b42b3eae57925e89faf00ce7327e3c733ac
MOHAMMED <arbibm55@gmail.com>
2024-10-02 17:11:02 +0100

    Create sign in API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,21 +1,47 @@
-import User from '../models/user.model.js';
-import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
- const signup = async (req, res,next) => {
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+
+
   const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username:username, email:email, password: hashedPassword });
-  try{
-await newUser.save();
-    res.status(201).json({ message: 'User created successfully' });
-   
-  }catch(error){
-     //res.status(500).json(error.message);//deleted
-    next(error)
-
+  const newUser = new User({
+    username: username,
+    email: email,
+    password: hashedPassword,
+  });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    //res.status(500).json(error.message);//deleted
+    next(error);
   }
-    
-   
-
 };
-export default signup 
+
+export const signin = async (req, res, next) => {
+  const {email, password } = req.body;
+
+  
+  
+  try {
+    const validUser = await User.findOne({email});
+    if (!validUser) return next(errorHandler(401, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+   
+    const expiresIn = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // 7 days from now
+
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiresIn })
+      .status(200)
+      .json(rest);
+    
+  } catch (error) {
+    next(error);
+  }
+};
