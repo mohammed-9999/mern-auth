f61dfde6de8c584283fad555dbcac8bdc97c5f39
MOHAMMED <arbibm55@gmail.com>
2024-10-05 18:48:42 +0100

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { persistor, store } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
  
     <Provider store={store}>
-    <App />
+        <PersistGate persistor={persistor} loading={null}>
+        <App />
+        </PersistGate>
+   
     </Provider>
     
  
